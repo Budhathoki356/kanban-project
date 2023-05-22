be0d9be2168deaf2cbd115e6b80502a39c99ef20
budhathoki356 <budhathoki1418@gmail.com>
2023-05-22 15:52:58 +0545

    feat: added api

--- frontend/app/models/Lane/index.js
+++ frontend/app/models/Lane/index.js
@@ -1,5 +1,17 @@
 import { Model } from "backbone";
+import api from "../../services";
 
-const LaneModel = Model.extend({});
+const LaneModel = Model.extend({
+  url() {
+    return `/lanes`;
+  },
+
+  updateLane({ id, title }) {
+    api.patch(`${this.url()}/${id}`, {
+      title: title,
+      id: id,
+    });
+  },
+});
 
 export default LaneModel;
