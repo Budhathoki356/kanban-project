7fdf87b1e9abefee7c394ff039fa25f8164f4467
budhathoki356 <budhathoki1418@gmail.com>
2023-05-29 11:48:41 +0545

    Add: Add Validation on Lane Form

--- frontend/app/views/Lane/NewLane/index.js
+++ frontend/app/views/Lane/NewLane/index.js
@@ -2,6 +2,7 @@ import template from "./template.pug";
 import { View } from "backbone.marionette";
 import { history } from "backbone";
 import lanesCollection from "../../../collections/Lane";
+import LaneModel from "../../../models/Lane";
 
 export default View.extend({
   className: "row mt-4",
@@ -11,16 +12,25 @@ export default View.extend({
   ui: {
     laneSaveBtn: ".btn-lane-save",
     laneTitleInput: ".lane-title",
+    errorMsg: ".text-danger",
+    form: 'form'
   },
 
   events: {
     "click @ui.laneSaveBtn": "saveLane",
   },
 
-  saveLane() {
-    lanesCollection.createLane({
-      title: this.ui.laneTitleInput.val(),
-    });
-    history.navigate("", { trigger: true });
+  saveLane(e) {
+    const laneModel = new LaneModel({ title: this.ui.laneTitleInput.val() });
+
+    if (laneModel.isValid()) {
+      lanesCollection.createLane({
+        title: this.ui.laneTitleInput.val(),
+      });
+      history.navigate("", { trigger: true });
+    } else {
+      e.preventDefault();
+      this.ui.errorMsg.text(laneModel.validationError);
+    }
   },
 });
