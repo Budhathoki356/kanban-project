c856761c3759778091c812c83d20b1e6020b6b9d
budhathoki356 <budhathoki1418@gmail.com>
2023-05-29 15:38:38 +0545

    refact: Refactor Card CSS

--- frontend/app/views/Card/index.js
+++ frontend/app/views/Card/index.js
@@ -1,20 +1,25 @@
+import { $ } from "backbone";
 import { View } from "backbone.marionette";
 import CardModel from "../../models/Card";
 import template from "./template.pug";
 
 export default View.extend({
-  className: "card-body bg-white mx-3 mt-3 mb-2 rounded k-card-body",
+  className: "card-body bg-white mx-3 mt-3 mb-2 rounded",
 
   template,
 
   ui: {
     cardDeleteBtn: '.card-delete-btn',
-    editCardBtn: '.card-edit-btn'
+    editCardBtn: '.card-edit-btn',
+    cardBtns: '.card-btns',
+    cardBody: '.k-card-body'
   },
 
   events: {
     'click @ui.cardDeleteBtn': "deleteCard",
     "click @ui.editCardBtn": "openCardModal",
+    "mouseenter @ui.cardBody": "displayCard",
+    "mouseleave @ui.cardBody": "hideCard",
   },
 
   templateContext() {
@@ -24,6 +29,14 @@ export default View.extend({
       description: this.model.get("description"),
       laneId: this.model.get("laneId")
     };
+  },
+
+  displayCard(e) {
+    $(e.currentTarget).find(".card-btns").removeClass("k-card_hide");
+  },
+
+  hideCard(e) {
+    $(e.currentTarget).find(".card-btns").addClass("k-card_hide");
   },
 
   modelEvents: {
